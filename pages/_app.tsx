0d42df12e1b2f820e32961c815e0c8df49a3417f
Shoichi Namba <alphabrend@gmail.com>
2021-12-05 15:36:44 +0900

    メタタグ更新

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,18 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { RecoilRoot } from 'recoil'
 import theme from '../styles/theme'
 import '../styles/globals.css'
+import React from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <RecoilRoot>
       <ChakraProvider theme={theme}>
+        <Head>
+          <title>Custom MyPage</title>
+          <meta name="description" content="My custom page for GitHub" />
+        </Head>
+
         <Component {...pageProps} />
       </ChakraProvider>
     </RecoilRoot>
